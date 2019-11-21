3d4723a627ba800ae271b441a93783adc8c68352
PauloQuintino <pauloqfs16@gmail.com>
2019-11-21 14:08:42 -0300

    .

--- src/main/java/br/com/sempreit_fortuno/pages/CartoesDeCreditoActivity.java
+++ src/main/java/br/com/sempreit_fortuno/pages/CartoesDeCreditoActivity.java
@@ -3,8 +3,12 @@ package br.com.sempreit_fortuno.pages;
 import br.com.smiles.base.PageBase;
 import br.com.smiles.base.VirtualElement;
 import br.com.smiles.helpers.LoggerHelper;
+import br.com.smiles.helpers.TxtReaderHelper;
 
 import static br.com.smiles.helpers.QueryHelper.getElementByXPath;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static br.com.smiles.helpers.QueryHelper.getElementById;
 
 import static br.com.smiles.base.DefaultBaseController.getPage_;
 
@@ -14,15 +18,75 @@ public class CartoesDeCreditoActivity extends PageBase {
 
 	LoggerHelper logger = new LoggerHelper(CartoesDeCreditoActivity.class);
 	UtilsMobile utils = getPage_(UtilsMobile.class);
-	
+
 	@SuppressWarnings("rawtype")
-	VirtualElement mensagemTelaCartoesDeCredito = getElementByXPath("//android.widget.TextView[@text='Cartões de crédito']");
-	
-	
+	String cartao = "//android.widget.RelativeLayout//android.widget.TextView[@text ='XXXX']",
+			dia = "//android.widget.TextView[contains(@text,'XX')]",
+			diaVencimento = "//android.widget.TextView[contains(@text,'XX')]";
+
+	VirtualElement mensagemTelaCartoesDeCredito = getElementByXPath(
+			"//android.widget.TextView[@text='Cartões de crédito']"),
+			btnAdicionarCartao = getElementByXPath("(//android.widget.ImageButton)[3]"),
+			limiteDoCartao = getElementById("com.impprove.app:id/edCreditLimit"),
+			tecladoLimite = getElementById("com.impprove.app:id/bt9"),
+			tecladoOK = getElementById("com.impprove.app:id/btOk"),
+			nomeDoCartao = getElementById("com.impprove.app:id/edDescription"),
+			selecaoCartao = getElementById("com.impprove.app:id/edBrand"),
+			bandeiraCartao,
+			dataFechamentoCartao = getElementById("com.impprove.app:id/edClosingDay"),
+			diaFechamentoFatura,
+			dataVencimentoCartao = getElementById("com.impprove.app:id/edDueDay"),
+			diaVencimentoCartao,
+			btnSalvar = getElementById("com.impprove.app:id/action_save"),
+			nomeCartaoSalvo = getElementById("com.impprove.app:id/tvDescription"),
+			limiteCartaoSalvo = getElementById("com.impprove.app:id/tvAvailableLimit");
+
 	public void validaTelaDeCartoesDeCredito() throws Exception {
 		utils.elementIsVisible(mensagemTelaCartoesDeCredito);
 		logger.info("Tela de " + mensagemTelaCartoesDeCredito.getText() + " apresentada");
 	}
-	
-	
+
+	public void adicionaCartaoDeCredito(String nomeCartao, String txtCartao, String diaFechamento,
+			String diaVencimentoCartaoCredito) throws Exception {
+
+		logger.debug("Inicio de Adicionar o Cartão de Crédito");
+
+		btnAdicionarCartao.click();
+		limiteDoCartao.click();
+
+		for (int i = 0; i <= 4; i++) {
+			tecladoLimite.click();
+		}
+		tecladoOK.click();
+		nomeDoCartao.sendKeys(nomeCartao);
+		selecaoCartao.click();
+		cartao = cartao.replace("XXXX", txtCartao);
+		logger.info(txtCartao);
+		bandeiraCartao = getElementByXPath(cartao);
+		bandeiraCartao.click();
+		dataFechamentoCartao.click();
+		dia = dia.replace("XX", diaFechamento);
+		diaFechamentoFatura = getElementByXPath(dia);
+		diaFechamentoFatura.click();
+		dataVencimentoCartao.click();
+		diaVencimento = diaVencimento.replace("XX", diaVencimentoCartaoCredito);
+		diaVencimentoCartao = getElementByXPath(diaVencimento);
+		diaVencimentoCartao.click();
+		btnSalvar.click();
+
+		logger.debug("Fim de Adicionar o Cartão de Crédito");
+
+	}
+
+	public void validaAdicionarCartaoDeCredito(String nomeCartao) throws Exception {
+		logger.debug("Início de validação do cadastro do cartão");
+
+		assertEquals(nomeCartao, nomeCartaoSalvo.getText());
+		logger.info("Apresentado o nome do cartão: " + nomeCartao);
+		assertTrue(elementIsVisible(limiteCartaoSalvo));
+		logger.info("Apresentado o limite do cartão: " + limiteCartaoSalvo.getText());
+
+		logger.debug("Fim de validação do cadastro do cartão");
+	}
+
 }
--- src/test/java/br/com/sempreit_fortuno/steps/EditarContasStep.java
+++ src/test/java/br/com/sempreit_fortuno/steps/EditarContasStep.java
@@ -1,7 +1,6 @@
 package br.com.sempreit_fortuno.steps;
 
 import static br.com.smiles.base.DefaultBaseController.getPage_;
-<<<<<<< HEAD
 import static br.com.smiles.helpers.DataTableHelper.getDt_;
 
 import br.com.sempreit_fortuno.pages.EditarContaActivity;
@@ -35,47 +34,6 @@ public class EditarContasStep {
 	@Então("devera validar os dados inseridos")
 	public void deveraValidarOsDadosInseridos() throws Exception{
 	    // Write code here that turns the phrase above into concrete actions
-	   
-=======
-
-import br.com.sempreit_fortuno.pages.EditarContaActivity;
-import br.com.sempreit_fortuno.pages.HomeActivity;
-import br.com.sempreit_fortuno.pages.IntroducaoActivity;
-import cucumber.api.java.es.Dado;
-import cucumber.api.java.pt.E;
-import cucumber.api.java.pt.Então;
-
-public class EditarContasStep {
-
-	HomeActivity home = getPage_(HomeActivity.class);
-	EditarContaActivity editaractivity = getPage_(EditarContaActivity.class);
-	IntroducaoActivity intro = getPage_(IntroducaoActivity.class);
-
-	@Dado("que a tela Contas foi apresentada")
-	public void queATelaContasFoiApresentada() throws Exception {	
-		intro.telaDePrimeiroAcesso();
-		home.validarHome();
-		home.acessarSaldoDeContas();
-
 	}
-
-	@E("que o usuário edite a carteira")
-	public void queOUsuárioEditeACarteira() throws Exception {
-		// Write code here that turns the phrase above into concrete actions
-
-	}
-
-	@E("que o usuário adicione os dados requisitados na tela")
-	public void queOUsuárioAdicioneOsDadosRequisitadosNaTela() throws Exception {
-		// Write code here that turns the phrase above into concrete actions
-
-	}
-
-	@Então("devera validar os dados inseridos")
-	public void deveraValidarOsDadosInseridos() throws Exception {
-		// Write code here that turns the phrase above into concrete actions
-
->>>>>>> branch 'master' of https://github.com/PietroBelliAguiar/app_fortuno.git
-	}
-
+	
 }
